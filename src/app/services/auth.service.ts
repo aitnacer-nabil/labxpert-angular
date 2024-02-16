1cb639cad8ff6bd04b69823f17852aa3ae8fa89a
aitnacer-nabil <palancia.gor@gmail.com>
2024-02-16 12:09:57 +0100

    add security

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {LoginRequest, LoginResponse} from "../models/LoginRequest";
+import {LoginRequest, LoginResponse} from "../models/login";
 import {BehaviorSubject, Observable, publish} from "rxjs";
 import {environment} from "../../environments/environment";
 import {JwtHelperService} from "@auth0/angular-jwt";
-import {LoggedUser} from "../models/loggeg-user.model";
+import {LoggedUser} from "../models/loggedUSer";
 import {Router} from "@angular/router";
 
 @Injectable({
@@ -12,7 +12,8 @@ import {Router} from "@angular/router";
 })
 export class AuthService {
   private baseUrl = environment.baseUrl;
-  jwtHelperService = new JwtHelperService()
+  jwtHelperService = new JwtHelperService();
+  tokenExpirationTimer:any;
 user = new BehaviorSubject<LoggedUser | null>(null);
   constructor(private httpClient: HttpClient, private router:Router) {
   }
@@ -25,23 +26,60 @@ user = new BehaviorSubject<LoggedUser | null>(null);
     return this.httpClient.post<LoginResponse>(`${this.baseUrl}login`, formData)
 
   }
-  public saveToken(jwtTokens:LoginResponse){
-    const decodedAccessToken= this.jwtHelperService.decodeToken(jwtTokens.accessToken);
-    const loggedUser = new LoggedUser(decodedAccessToken.sub,decodedAccessToken.roles,jwtTokens.accessToken,this.getExpiration(decodedAccessToken.exp))
+  saveToken(jwtTokens:LoginResponse){
+    const decodedAccessToken=this.jwtHelperService.decodeToken(jwtTokens.accessToken);
+    const loggedUser = new LoggedUser(decodedAccessToken.sub,decodedAccessToken.roles,jwtTokens.accessToken,this.getExpirationDate(decodedAccessToken.exp));
     this.user.next(loggedUser);
-    console.log(decodedAccessToken);
-    console.log(loggedUser);
-    this.redirectLoggedInUse(decodedAccessToken,jwtTokens.accessToken)
-  }
-  redirectLoggedInUse(decodedToken: any, accessToken: string){
-    if(decodedToken.roles.includes("TECHNICIEN")) this.router.navigateByUrl("/");
-    else if(decodedToken.roles.includes("RESPONSABLE")){
+    this.autologout(this.getExpirationDate(decodedAccessToken.exp).valueOf()-new Date().valueOf());
+    localStorage.setItem("userData",JSON.stringify(loggedUser));
 
-    }
+    this.redirectLoggedInUser(decodedAccessToken,jwtTokens.accessToken);
+    console.log(loggedUser);
   }
-  getExpiration(exp:number){
-    const date = new Date(0);
+  getExpirationDate(exp:number){
+    const date=new Date(0);
     date.setUTCSeconds(exp);
     return date;
+  }
+  redirectLoggedInUser(decodedToken:any,accessToken:string){
+    if(decodedToken.roles.includes("Responsable"))
+      this.router.navigateByUrl("/");
+    else if(decodedToken.roles.includes("Technicien"))
+      this.router.navigateByUrl("/echantillon/all")
+  }
+  autoLogin(){
+    const userDataString: string | null = localStorage.getItem('userData');
+
+    // @ts-ignore
+    if (userDataString !== null) {
+      const userData: {
+        username: string,
+        roles: string[],
+        _token: string,
+        _expiration: Date
+      } = JSON.parse(userDataString);
+      if(!userData)return;
+      const  loadedUser=new LoggedUser(userData.username,userData.roles,userData._token,new Date(userData._expiration));
+      if(loadedUser.token){
+        this.user.next(loadedUser);
+        this.autologout(loadedUser._expiration.valueOf()- new Date().valueOf());
+      }
+      // Now you can use userData safely
+    }
+
+  }
+  logout(){
+    localStorage.clear();
+    this.user.next(null);
+    this.router.navigate(['/auth'])
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer=null;
+  }
+  autologout(expirationDuration:number){
+    this.tokenExpirationTimer= setTimeout(()=>{
+      this.logout();
+    },expirationDuration)
   }
 }
